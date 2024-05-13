343d7ca4cea21510c8a63966ffdc2dab7dd9a47c
yufeng66 <43092209+yufeng66@users.noreply.github.com>
2024-05-13 18:33:59 -0400

    simple2
    a simple controller that is a linear combination of target, current and roll

--- controllers.py
+++ controllers.py
@@ -12,8 +12,12 @@ class SimpleController(BaseController):
   def update(self, target_lataccel, current_lataccel, state):
     return (target_lataccel - current_lataccel) * 0.3
 
+class SimpleController2(BaseController):
+  def update(self, target_lataccel, current_lataccel, state):
+    return 0.5*target_lataccel - 0.1*current_lataccel - 0.35* state.roll_lataccel
 
 CONTROLLERS = {
   'open': OpenController,
   'simple': SimpleController,
+  'simple2': SimpleController2, 
 }
